39820a7dd5a09c30f74aa0bcb9f05c320ba51f88
Galina Netishina <netishinagalina@yandex.ru>
2024-10-21 22:09:25 +0300

    star rework

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-
-import './App.css'
 import Stars from "./components/Stars";
 
 function App() {  
 
   return (
     <>   
-    <Stars/>
     <Stars count={1}/>
     <Stars count={3}/>
     <Stars count={5}/>
-    <Stars count={7}/>
     </>
   )
 }
--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -2,12 +2,10 @@ import "./Stars.css"
 
 const Star = () => {
   return (
-    <li>
-        <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
-            <path d="M0 0h18v18H0z" fill="none"/>
-        </svg>
-    </li>
+    <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
+        <path d="M0 0h18v18H0z" fill="none"/>
+    </svg>   
   )
 }
 
@@ -19,11 +17,16 @@ type TStarCountProps = {
 
 export default function Stars({count=0}: TStarCountProps ) {
     const stars: number[] = Array.from(Array(count).keys()).map(i => i + 1);
-  if (count < 1 || count > 5) {
-    return null;}
+  
   return (
     <ul className="card-body-stars clearfix">
-    {stars.map((i: number) => { return <Star key={i}/>})
+    {stars.map((i: number) => { 
+      return (
+      <li key={`star${i}`}>
+      <Star/>
+      </li>
+      )
+    })
     } 
     </ul>
   )
